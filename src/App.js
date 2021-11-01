ae3c255c1cfac8da39b7f9ec3cb898e24f09a1ac
zacwoll <zacwoll@gmail.com>
2021-11-01 17:38:23 -0500

    Working on 0x02 react intro!

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="Holberton Logo: Red Seahorse"/>
+        <h1>School dashboard</h1>
+      </div>
+        <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="form">
+          <label htmlFor="email">
+            <span>Email:</span>
+            <input type="email" name="email" id="email" />
+          </label>
+
+          <label htmlFor="password">
+            <span>Password:</span>
+            <input type="password" name="password" id="pwd" />
+          </label>
+
+          <button onClick={() => {}}>OK</button>
+        </div>
+      </div>
+        <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
